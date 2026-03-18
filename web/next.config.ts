48e51f82dddb177c1a1c3777fa4e64219302a023
isarikaya <isarikayaa@outlook.com>
2026-03-18 19:19:26 +0300

    add opennext

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  devIndicators: false
-};
+  devIndicators: false,
+}
 
-export default nextConfig;
+export default nextConfig
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare"
+initOpenNextCloudflareForDev()
